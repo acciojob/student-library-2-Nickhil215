d77e6d6ce308656908faa010815f3447c4041363
nikhil vanamala <nickhil215@gmail.com>
2023-01-27 16:43:22 +0530

    mkf

--- src/main/java/com/driver/services/BookService.java
+++ src/main/java/com/driver/services/BookService.java
@@ -7,6 +7,7 @@ import com.driver.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,6 +22,10 @@ public class BookService {
     public void createBook(Book book){
         if(book != null && book.getAuthor() != null) { //Checking if the book and its author are not null
             Author author = book.getAuthor();
+//Checking if the author already has a list of books, if not, creating a new list
+            if(author.getBooksWritten() == null) {
+                author.setBooksWritten(new ArrayList<>());
+            }
             author.getBooksWritten().add(book);
             book.setAuthor(author);
             book.setAvailable(true);
